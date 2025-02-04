8a02d89adf92240cbb664e75e84098a376b915ab
ma310kano <ma310kano@gmail.com>
2025-02-04 22:22:21 +0900

    人間の採集アイテムを限定するように変更

--- Life/Application/Data/HumanData.cs
+++ Life/Application/Data/HumanData.cs
@@ -7,8 +7,9 @@
 /// <param name="FirstName">名</param>
 /// <param name="Family">家族</param>
 /// <param name="Area">エリア</param>
+/// <param name="GatheringItems">採集アイテムのコレクション</param>
 /// <param name="BuildingRecipes">建造物レシピのコレクション</param>
 /// <param name="ItemRecipes">アイテムレシピのコレクション</param>
 /// <param name="EquipmentItems">装備アイテムのコレクション</param>
 /// <param name="InventorySlots">インベントリースロットのコレクション</param>
-public record class HumanData(string HumanId, string FirstName, FamilySummaryData Family, AreaSummaryData Area, IReadOnlyCollection<BuildingRecipeSummaryData> BuildingRecipes, IReadOnlyCollection<ItemRecipeSummaryData> ItemRecipes, IReadOnlyCollection<ItemSummaryData> EquipmentItems, IReadOnlyCollection<InventorySlotData> InventorySlots);
+public record class HumanData(string HumanId, string FirstName, FamilySummaryData Family, AreaSummaryData Area, IReadOnlyCollection<ItemSummaryData> GatheringItems, IReadOnlyCollection<BuildingRecipeSummaryData> BuildingRecipes, IReadOnlyCollection<ItemRecipeSummaryData> ItemRecipes, IReadOnlyCollection<ItemSummaryData> EquipmentItems, IReadOnlyCollection<InventorySlotData> InventorySlots);
