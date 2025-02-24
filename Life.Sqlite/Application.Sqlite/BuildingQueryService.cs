51911903a62d82520c387753f6479176b4b2c319
ma310kano <ma310kano@gmail.com>
2025-02-24 11:52:51 +0900

    作業台(木工)のページを作成

--- Life.Sqlite/Application.Sqlite/BuildingQueryService.cs
+++ Life.Sqlite/Application.Sqlite/BuildingQueryService.cs
@@ -33,7 +33,9 @@ public class BuildingQueryService(IConfiguration configuration) : IBuildingQuery
 		using IDbConnection connection = ConnectionFactory.Create();
 		connection.Open();
 
-		const string sql = @"SELECT
+		BuildingRecord source;
+		{
+			const string sql = @"SELECT
 	  bui.building_id
 	, bnm.building_name
 FROM
@@ -44,13 +46,42 @@ FROM
 WHERE
 	bui.building_id = :building_id";
 
-		var param = new
-		{
-			building_id = buildingId,
-			language_code = _languageCode,
-		};
+			var param = new
+			{
+				building_id = buildingId,
+				language_code = _languageCode,
+			};
 
-		BuildingData result = connection.QuerySingle<BuildingData>(sql, param);
+			source = connection.QuerySingle<BuildingRecord>(sql, param);
+		}
+
+		ItemRecipeSummaryData[] itemRecipes;
+		{
+			const string sql = @"SELECT
+	  irc.item_recipe_id
+	, inm.item_name
+FROM
+	item_recipes irc
+	INNER JOIN items ite
+		ON irc.item_id = ite.item_id
+	INNER JOIN item_names inm
+		ON  ite.item_id = inm.item_id
+		AND inm.language_code = :language_code
+WHERE
+	irc.building_id = :building_id
+ORDER BY
+	irc.item_recipe_id";
+
+			var param = new
+			{
+				building_id = buildingId,
+				language_code = _languageCode,
+			};
+
+			itemRecipes = connection.Query<ItemRecipeSummaryData>(sql, param).ToArray();
+		}
+
+		BuildingData result = new(source.BuildingId, source.BuildingName, itemRecipes);
 
 		return result;
 	}
@@ -64,6 +95,17 @@ WHERE
 	{
 		return Task.Run(() => QuerySingle(buildingId));
 	}
+
+	#endregion
+
+	#region Nested types
+
+	/// <summary>
+	/// 建造物のレコード
+	/// </summary>
+	/// <param name="BuildingId">建造物ID</param>
+	/// <param name="BuildingName">建造物名</param>
+	private record class BuildingRecord(string BuildingId, string BuildingName);
 
 	#endregion
 }
--- Life/Application/Data/BuildingData.cs
+++ Life/Application/Data/BuildingData.cs
@@ -5,4 +5,5 @@
 /// </summary>
 /// <param name="BuildingId">建造物ID</param>
 /// <param name="BuildingName">建造物名</param>
-public record class BuildingData(string BuildingId, string BuildingName);
+/// <param name="ItemRecipes">アイテムレシピのコレクション</param>
+public record class BuildingData(string BuildingId, string BuildingName, IReadOnlyCollection<ItemRecipeSummaryData> ItemRecipes);
